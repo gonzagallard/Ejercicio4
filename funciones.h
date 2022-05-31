1667e179eb3842c479bb7c32d1db9d05f33c432d
gonzagallard <ggallardo@fi.uba.ar>
2022-05-31 06:31:35 -0300

    ordenado_1

--- funciones.h
+++ funciones.h
@@ -2,12 +2,6 @@
 #define FUNCIONES_H
 
 
-typedef uint8_t color_t;
-
-color_t color_crear(bool r, bool g, bool b);
-
-void color_a_rgb(color_t c, uint8_t *r, uint8_t *g, uint8_t *b);
-
 const char* figura_tipo_a_cadena(figura_tipo_t figura);
 
 bool leer_encabezado_figura(FILE *f, char nombre[], figura_tipo_t *tipo, bool *infinito, size_t *cantidad_polilineas);
