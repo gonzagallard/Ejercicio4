8a0f9d47d97c4d14fdd93c39f35b8a333b2e786e
gonzagallard <ggallardo@fi.uba.ar>
2022-06-03 03:55:22 -0300

    update compilar.sh

--- compilar.sh
+++ compilar.sh
@@ -1,3 +1,3 @@
-gcc -Wall -std=c99 -pedantic -g -c main.c
-gcc -Wall -std=c99 -pedantic -g -c funciones.c
-gcc -Wall -std=c99 -pedantic -g main.o funciones.o -o main
+gcc -Wall -std=c99 -pedantic -g -c ./build/main.c
+gcc -Wall -std=c99 -pedantic -g -c ./build/funciones.c
+gcc -Wall -std=c99 -pedantic -g main.o funciones.o -o ./build/main
